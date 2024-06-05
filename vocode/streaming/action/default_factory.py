f0a694478bd82f8c506387267660ca20fac9e51d
srhinos <6531393+srhinos@users.noreply.github.com>
2024-06-05 08:30:00 -0400

    Fixes

--- vocode/streaming/action/default_factory.py
+++ vocode/streaming/action/default_factory.py
@@ -1,8 +1,8 @@
 from typing import Dict, Sequence, Type
 
-from vocode.meta import is_extra_installed
 from vocode.streaming.action.abstract_factory import AbstractActionFactory
 from vocode.streaming.action.base_action import BaseAction
+from vocode.streaming.action.dtmf import TwilioDTMF, VonageDTMF
 from vocode.streaming.action.end_conversation import EndConversation
 from vocode.streaming.action.execute_external_action import ExecuteExternalAction
 from vocode.streaming.action.phone_call_action import (
@@ -10,6 +10,7 @@ from vocode.streaming.action.phone_call_action import (
     VonagePhoneConversationAction,
 )
 from vocode.streaming.action.record_email import RecordEmail
+from vocode.streaming.action.transfer_call import TwilioTransferCall, VonageTransferCall
 from vocode.streaming.action.wait import Wait
 from vocode.streaming.models.actions import ActionConfig, ActionType
 
@@ -20,19 +21,15 @@ CONVERSATION_ACTIONS: Dict[ActionType, Type[BaseAction]] = {
     ActionType.EXECUTE_EXTERNAL_ACTION: ExecuteExternalAction,
 }
 
-if is_extra_installed("telephony"):
-    from vocode.streaming.action.dtmf import TwilioDTMF, VonageDTMF
-    from vocode.streaming.action.transfer_call import TwilioTransferCall, VonageTransferCall
+VONAGE_ACTIONS: Dict[ActionType, Type[VonagePhoneConversationAction]] = {
+    ActionType.TRANSFER_CALL: VonageTransferCall,
+    ActionType.DTMF: VonageDTMF,
+}
 
-    VONAGE_ACTIONS: Dict[ActionType, Type[VonagePhoneConversationAction]] = {
-        ActionType.TRANSFER_CALL: VonageTransferCall,
-        ActionType.DTMF: VonageDTMF,
-    }
-
-    TWILIO_ACTIONS: Dict[ActionType, Type[TwilioPhoneConversationAction]] = {
-        ActionType.TRANSFER_CALL: TwilioTransferCall,
-        ActionType.DTMF: TwilioDTMF,
-    }
+TWILIO_ACTIONS: Dict[ActionType, Type[TwilioPhoneConversationAction]] = {
+    ActionType.TRANSFER_CALL: TwilioTransferCall,
+    ActionType.DTMF: TwilioDTMF,
+}
 
 
 class DefaultActionFactory(AbstractActionFactory):
